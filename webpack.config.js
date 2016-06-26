e53503c2fa7937fd00ca73b04a92d859e10f7eba
Jose Alves <jose.tapadas@gmail.com>
2016-06-26 23:56:56 +0100

    ReactJS Hello World
    - initial webpack config
    - added babel
    - added both run and prod scripts to package.json

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var HTMLWebPackPlugin = require('html-webpack-plugin');
+var HTMLWebPackPluginConfig = new HTMLWebPackPlugin({
+    template: __dirname + '/app/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry: [
+      './app/index.js'
+    ],
+    output: {
+      path: __dirname + '/dist',
+      filename: 'index_bundle.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }
+      ]
+    },
+    plugins: [
+      HTMLWebPackPluginConfig
+    ]
+}
