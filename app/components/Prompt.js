7e8cabb5f3168f8f2116657646df181469d797fe
José Alves <jose.tapadas@gmail.com>
2016-07-04 10:05:52 +0100

    transformed Prompt into a stateless function component

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -1,35 +1,35 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 
-var Prompt = React.createClass({
-  propTypes: {
-      header: PropTypes.string.isRequired,
-      onUpdateUser: PropTypes.func.isRequired,
-      onSubmitUser: PropTypes.func.isRequired,
-      username: PropTypes.string.isRequired
-  },
-  render: function() {
-    return (
-      <div>
-        <h2>{this.props.header}</h2>
-        <form onSubmit={this.props.onSubmitUser}>
-          <div>
-            <input
-              placeholder="GitHub username"
-              type="text"
-              value={this.props.username}
-              onChange={this.props.onUpdateUser}/>
-          </div>
-          <div>
-            <button
-              type="submit">
-              Continue
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-});
+// stateless function component
+function Prompt(props) {
+  return (
+    <div>
+      <h2>{props.header}</h2>
+      <form onSubmit={props.onSubmitUser}>
+        <div>
+          <input
+            placeholder="GitHub username"
+            type="text"
+            value={props.username}
+            onChange={props.onUpdateUser}/>
+        </div>
+        <div>
+          <button
+            type="submit">
+            Continue
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+Prompt.propTypes = {
+  header: PropTypes.string.isRequired,
+  onUpdateUser: PropTypes.func.isRequired,
+  onSubmitUser: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired
+};
 
 module.exports = Prompt;
